1349721c251cc7a09d2ba79d7ab12e8683a8d718
Orhan Hirsch <orhanhenrik@gmail.com>
2019-04-01 12:24:18 +0200

    use postgres create database with template instead of pg_restore

--- index.js
+++ index.js
@@ -7,43 +7,47 @@ const pg_host = process.env.PG_HOST;
 const pg_database = process.env.PG_DATABASE;
 const pg_username = process.env.PG_USERNAME;
 
-console.log(`Starting lego-cypress-helper with PG_HOST=${pg_host}, PG_DATABASE=${pg_database}, PG_USERNAME=${pg_username}`);
+console.log(
+  `Starting lego-cypress-helper with PG_HOST=${pg_host}, PG_DATABASE=${pg_database}, PG_USERNAME=${pg_username}`
+);
 
 const backup = () => {
-    const cmd = `pg_dump -h ${pg_host} -U ${pg_username} --clean ${pg_database} | grep -v idle_in_transaction_session_timeout > dump`;
-    console.log(`Running: ${cmd}`)
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        console.error('Error while running pg_dump');
-        console.error(err.toString());
-        return;
-      }
+  const pg_commands = `select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where pg_stat_activity.datname='${pg_database}_copy'; drop database if exists ${pg_database}_copy; create database ${pg_database}_copy with template ${pg_database};`;
+  const cmd = `echo "${pg_commands}" | psql -h ${pg_host} -U ${pg_username} postgres`;
+  console.log(`Running: ${cmd}`);
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.error('Error while running db copy');
+      console.error(err.toString());
+      return;
+    }
 
-      console.log('pg_dump was generated');
-    });
+    console.log('db copy was generated');
+  });
 };
 
 app.get('/', (req, res) => res.send('OK'));
 app.post('/', (req, res) => {
-    console.log('post to lego-cypress-helper');
+  console.log('post to lego-cypress-helper');
 
-    // const cmd = `pg_restore -h ${pg_host} -U ${pg_username} --clean --dbname=${pg_database} dump`;
-    const cmd = `psql -h ${pg_host} -U ${pg_username} -d ${pg_database} < dump`;
+  const pg_commands = `select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where pg_stat_activity.datname='${pg_database}'; drop database if exists ${pg_database}; create database ${pg_database} with template ${pg_database}_copy;`;
+  const cmd = `echo "${pg_commands}" | psql -h ${pg_host} -U ${pg_username} postgres`;
 
-    console.log(`Running: ${cmd}`)
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        console.error('Error while running pg_restore');
-        console.error(err.toString());
-        res.status(500).send('ERROR');
-        return;
-      }
+  console.log(`Running: ${cmd}`);
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.error('Error while restoring db');
+      console.error(err.toString());
+      res.status(500).send('ERROR');
+      return;
+    }
 
-      console.log('Successfully restored');
-      res.send('OK');
-    });
-})
+    console.log('Successfully restored');
+    res.send('OK');
+  });
+});
 
 backup();
-app.listen(port, () => console.log(`lego-cypress-helper listening on port ${port}!`))
-
+app.listen(port, () =>
+  console.log(`lego-cypress-helper listening on port ${port}!`)
+);
